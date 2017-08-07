6f0ab011f7d8ea37eb8e1da03d3beef27a1eccf9
Sergey Piterskikh <piterskikhsa@mail.ru>
2017-08-07 22:24:49 +0300

    Wrote four functions

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Github Trends
 
-[TODO. There will be project description]
+The script finds 20 repositories created in the last week, selects the repositories with the largest number of stars, 
+and for each count the number of open questions. And displays them along with the links.
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+
+python github_trending.py
+
+dthree - mailit 1739 
+7
+Optional SMTP Auth https://api.github.com/repos/dthree/mailit/issues/9
+No SMTP auth https://api.github.com/repos/dthree/mailit/issues/8
+Greeting never received https://api.github.com/repos/dthree/mailit/issues/7
+Does it support Self-signed certificates and CRAM-MD5 auth? https://api.github.com/repos/dthree/mailit/issues/6
+Not reading from the config.json https://api.github.com/repos/dthree/mailit/issues/5
+What have you used for interactive api docs? https://api.github.com/repos/dthree/mailit/issues/2
+Does it support file attachments? https://api.github.com/repos/dthree/mailit/issues/1
+
+```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,44 @@
+import requests
+import datetime
+
+url = 'https://api.github.com'
+
+
+def get_json_response(url, params):
+    response = requests.get(url, params)
+    return response.json()
 
 
 def get_trending_repositories(top_size):
-    pass
+    last_week = str(datetime.date.today() - datetime.timedelta(days=7))
+    search_repo_url = '{url}/search/repositories?q=created:>={date}&sort=stars'.format(url=url, date=last_week)
+    url_params = {'q': 'created:>=' + last_week, 'sort': 'stars'}
+    return get_json_response(search_repo_url, url_params)['items'][:top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    issues_url = '{url}/repos/{owner}/{name}/issues'.format(url=url, owner=repo_owner, name=repo_name)
+    url_params = {'state': 'open'}
+    issues_list = get_json_response(issues_url, url_params)
+    for issue in issues_list:
+        print(issue['title'], issue['url'])
+
+
+def main():
+    top_size = 20
+    trending_rep = get_trending_repositories(top_size)
+    for repository in trending_rep:
+        repo_owner = repository['owner']['login']
+        repo_name = repository['name']
+        print('-------------')
+        print(repo_owner, '-', repo_name, repository['stargazers_count'])
+
+        if repository['open_issues_count'] > 0:
+            print(repository['open_issues_count'])
+            get_open_issues_amount(repo_owner, repo_name)
+        else:
+            print('Открытых вопросов нет')
+
 
 if __name__ == '__main__':
-    pass
+    main()
