58aabc183ececfbb316e68e3a9d735cffb55e3ca
Sergey Piterskikh <piterskikhsa@mail.ru>
2017-08-08 22:05:25 +0300

    updated functions and global variable

--- github_trending.py
+++ github_trending.py
@@ -1,8 +1,6 @@
 import requests
 import datetime
 
-url = 'https://api.github.com'
-
 
 def get_json_response(url, params):
     response = requests.get(url, params)
@@ -11,22 +9,23 @@ def get_json_response(url, params):
 
 def get_trending_repositories(top_size):
     last_week = str(datetime.date.today() - datetime.timedelta(days=7))
-    search_repo_url = '{url}/search/repositories?q=created:>={date}&sort=stars'.format(url=url, date=last_week)
-    url_params = {'q': 'created:>=' + last_week, 'sort': 'stars'}
-    return get_json_response(search_repo_url, url_params)['items'][:top_size]
+    search_repo_url = 'https://api.github.com/search/repositories'
+    url_params = {'q': 'created:>=' + last_week, 'sort': 'stars', 'per_page':top_size}
+
+    return get_json_response(search_repo_url, url_params)['items']
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    issues_url = '{url}/repos/{owner}/{name}/issues'.format(url=url, owner=repo_owner, name=repo_name)
+    issues_url = 'https://api.github.com/repos/{owner}/{name}/issues'.format(owner=repo_owner, name=repo_name)
     url_params = {'state': 'open'}
     issues_list = get_json_response(issues_url, url_params)
-    for issue in issues_list:
-        print(issue['title'], issue['url'])
+    return issues_list
 
 
 def main():
     top_size = 20
     trending_rep = get_trending_repositories(top_size)
+
     for repository in trending_rep:
         repo_owner = repository['owner']['login']
         repo_name = repository['name']
@@ -35,7 +34,12 @@ def main():
 
         if repository['open_issues_count'] > 0:
             print(repository['open_issues_count'])
-            get_open_issues_amount(repo_owner, repo_name)
+            try:
+                issues_list = get_open_issues_amount(repo_owner, repo_name)
+                for issue in issues_list:
+                    print(issue['title'], issue['url'])
+            except TypeError:
+                print('ОШИБКА:  - Превышен лимит подключений, попробуйте завтра')
         else:
             print('Открытых вопросов нет')
 
